38673224e5dca1776984a4e9e92422ac6d54ca91
Mikhail Guzhov <112636452+MikhailGuzhov@users.noreply.github.com>
2024-10-05 18:02:23 +0300

    Update main.js

--- main.js
+++ main.js
@@ -26,72 +26,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const transliterate = (text) => {
     const ruToEn = {
-      а: "a",
-      б: "b",
-      в: "v",
-      г: "g",
-      д: "d",
-      е: "e",
-      ё: "e",
-      ж: "zh",
-      з: "z",
-      и: "i",
-      й: "y",
-      к: "k",
-      л: "l",
-      м: "m",
-      н: "n",
-      о: "o",
-      п: "p",
-      р: "r",
-      с: "s",
-      т: "t",
-      у: "u",
-      ф: "f",
-      х: "kh",
-      ц: "ts",
-      ч: "ch",
-      ш: "sh",
-      щ: "shch",
-      ъ: "",
-      ы: "y",
-      ь: "",
-      э: "e",
-      ю: "yu",
-      я: "ya",
-      А: "A",
-      Б: "B",
-      В: "V",
-      Г: "G",
-      Д: "D",
-      Е: "E",
-      Ё: "E",
-      Ж: "Zh",
-      З: "Z",
-      И: "I",
-      Й: "Y",
-      К: "K",
-      Л: "L",
-      М: "M",
-      Н: "N",
-      О: "O",
-      П: "P",
-      Р: "R",
-      С: "S",
-      Т: "T",
-      У: "U",
-      Ф: "F",
-      Х: "Kh",
-      Ц: "Ts",
-      Ч: "Ch",
-      Ш: "Sh",
-      Щ: "Shch",
-      Ъ: "",
-      Ы: "Y",
-      Ь: "",
-      Э: "E",
-      Ю: "Yu",
-      Я: "Ya",
+      а: "a", б: "b", в: "v", г: "g", д: "d",
+      е: "e", ё: "e", ж: "zh", з: "z", и: "i",
+      й: "y", к: "k", л: "l", м: "m", н: "n",
+      о: "o", п: "p", р: "r", с: "s", т: "t",
+      у: "u", ф: "f", х: "kh", ц: "ts", ч: "ch",
+      ш: "sh", щ: "shch", ъ: "", ы: "y", ь: "",
+      э: "e", ю: "yu", я: "ya", А: "A", Б: "B",
+      В: "V", Г: "G", Д: "D", Е: "E", Ё: "E",
+      Ж: "Zh", З: "Z", И: "I", Й: "Y", К: "K",
+      Л: "L", М: "M", Н: "N", О: "O", П: "P",
+      Р: "R", С: "S", Т: "T", У: "U", Ф: "F",
+      Х: "Kh", Ц: "Ts", Ч: "Ch", Ш: "Sh", Щ: "Shch",
+      Ъ: "", Ы: "Y", Ь: "", Э: "E", Ю: "Yu", Я: "Ya",
     };
     return text
       .split("")
@@ -144,10 +91,11 @@ document.addEventListener("DOMContentLoaded", () => {
       highlightDuplicate(duplicateNum);
     });
 
-    const totalMatches = matches.length;
+    const uniqueMatches = Array.from(new Set(matches)); // Уникальные совпадения
+    const totalMatches = uniqueMatches.length;
     const totalDuplicates = duplicates.size + inputNumbersDuplicates.length;
 
-    matchesDiv.innerHTML = "Совпадения: " + matches.join(", ");
+    matchesDiv.innerHTML = "Совпадения: " + uniqueMatches.join(", ");
     statsDiv.innerHTML = `Общее количество совпадений: ${totalMatches} <br> Количество дубликатов: ${totalDuplicates}`;
     downloadButton.style.display = totalMatches > 0 ? "block" : "none";
     resetButton.style.display = "block";
